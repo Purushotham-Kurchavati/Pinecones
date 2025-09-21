d1fc80a59f6c1e06c004e2d6d885d4d1d1715d3e
purushotham “Events-Meetup-Hackathon” <kurchavati.purushotham@gmail.com>
2025-09-21 01:27:54 +0000

    Set up Firebase Backend

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -1,11 +1,11 @@
-import { PineCone } from 'lucide-react';
+import { PinIcon as Pinecone } from 'lucide-react';
 import Link from 'next/link';
 import { cn } from '@/lib/utils';
 
 export function Logo({ className }: { className?: string }) {
   return (
     <Link href="/" className={cn("flex items-center space-x-2", className)}>
-      <PineCone className="h-6 w-6 text-primary" />
+      <Pinecone className="h-6 w-6 text-primary" />
       <span className="font-bold font-headline text-lg hidden sm:inline-block">
         Pinecones
       </span>
