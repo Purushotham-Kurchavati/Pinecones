b2d1f07673b92ba120b5bb90b1e62096f8e6f890
purushotham “Events-Meetup-Hackathon” <kurchavati.purushotham@gmail.com>
2025-09-21 03:04:46 +0000

    it not sloved pls let me do it how to get the API KEy

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -7,8 +7,8 @@ const firebaseConfig = {
   authDomain: "studio-6517283771-3fc00.firebaseapp.com",
   projectId: "studio-6517283771-3fc00",
   storageBucket: "studio-6517283771-3fc00.appspot.com",
-  messagingSenderId: "1758416904232",
-  appId: "1:1758416904232:web:7f6a7071018507851b22e7"
+  messagingSenderId: "175841690423",
+  appId: "1:175841690423:web:7f6a7071018507851b22e7"
 };
 
 // Initialize Firebase
