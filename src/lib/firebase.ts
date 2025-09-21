774efea344bbe803ba39e02ffc88d78deebdeeb8
purushotham “Events-Meetup-Hackathon” <kurchavati.purushotham@gmail.com>
2025-09-21 02:59:28 +0000

    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY!,   authDomain: process

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,12 +3,12 @@ import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY!,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN!,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID!,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET!,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID!,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID!,
+  apiKey: "AIzaSydsL5s-g1i3d3_sassycheese-iWi6_5Xjsjc",
+  authDomain: "studio-6517283771-3fc00.firebaseapp.com",
+  projectId: "studio-6517283771-3fc00",
+  storageBucket: "studio-6517283771-3fc00.appspot.com",
+  messagingSenderId: "1758416904232",
+  appId: "1:1758416904232:web:7f6a7071018507851b22e7"
 };
 
 // Initialize Firebase
